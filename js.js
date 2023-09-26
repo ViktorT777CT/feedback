2a93bbe15606a77f9c621be5df3b87f3f7a87ebb
@v.prasolov <p_viktor91@mail.ru>
2023-09-26 17:17:47 +0600

    добавлена ширина

--- js.js
+++ js.js
@@ -8,26 +8,36 @@ $("#add").click(function(){
 
     $("#form1 .container_form:last").append(
 
-        '<div class="d-flex">'+
-            '<div class="mb-3 p-2">'+
-                '<span class=”error_message”></span>'+
-                '<input value="'+len+'" type="text" name="number" class="form-control bg_color" id="inputNumber'+len+'" readonly>'+
+        '<div class="row">'+
+            '<div class="col-1 p-0">'+
+                '<div class="p-1">'+
+                    '<span class=”error_message”></span>'+
+                    '<input value="'+len+'" type="text" name="number" class="form-control bg_color" id="inputNumber'+len+'" readonly>'+
+                '</div>'+
             '</div>'+
-            '<div class="mb-3 p-2">'+
-                '<span class=”error_message”></span>'+
-                '<input type="text" name="article" class="form-control bg_color" id="inputArticle'+len+'">'+
+            '<div class="col-3 p-0">'+
+                '<div class="p-1">'+
+                    '<span class=”error_message”></span>'+
+                    '<input type="text" name="article" class="form-control bg_color" id="inputArticle'+len+'">'+
+                '</div>'+
             '</div>'+
-            '<div class="mb-3 p-2">'+
-                '<span class=”error_message”></span>'+
-                '<input type="text" name="name" class="form-control bg_color" id="inputName'+len+'">'+
+            '<div class="col-2 p-0">'+
+                '<div class="p-1">'+
+                    '<span class=”error_message”></span>'+
+                    '<input type="text" name="name" class="form-control bg_color" id="inputName'+len+'">'+
+                '</div>'+
             '</div>'+
-            '<div class="mb-3 p-2">'+
-                '<span class=”error_message”></span>'+
-                '<input type="text" name="quantity" class="form-control bg_color" id="inputQuantity'+len+'">'+
+            '<div class="col-1 p-0">'+
+                '<div class="p-1">'+
+                    '<span class=”error_message”></span>'+
+                    '<input type="text" name="quantity" class="form-control bg_color" id="inputQuantity'+len+'">'+
+                '</div>'+
             '</div>'+
-            '<div class="mb-3 p-2">'+
-                '<span class=”error_message”></span>'+
-                '<input type="text" name="comment" class="form-control bg_color" id="inputComment'+len+'">'+
+            '<div class="col p-0">'+
+                '<div class="p-1">'+
+                    '<span class=”error_message”></span>'+
+                    '<input type="text" name="comment" class="form-control bg_color" id="inputComment'+len+'">'+
+                '</div>'+
             '</div>'+
         '</div>'
 
